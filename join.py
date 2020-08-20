29eaf40e7ec5aa53b40085c8c385118ad610ab9f
ImpulseAI <68378591+ImpulseAI@users.noreply.github.com>
2020-08-20 19:56:51 +0530

    join.py

---
+++ join.py
@@ -0,0 +1,18 @@
+
+import os
+def join(files, dest_file):
+    output_file = open(dest_file, 'wb')
+    for file in files:
+        print('Joining',file,'...')
+        input_file = open(file, 'rb')
+        while True:
+            byte = input_file.read(1000000)
+            if not byte:
+                break
+            output_file.write(byte)
+        input_file.close()
+    output_file.close()
+    for file in files:
+        print('Removing',file,'...')
+        os.remove(file)
+join(['zeowx.zip.000.COOL'],'zeowx.zip')
